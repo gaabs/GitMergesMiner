dd26d65b3fbb057fd1655f103467eb78e08d0968
gaabs <gaabs@cin.ufpe.br>
2018-09-23 19:04:36 -0300

    temp

--- src/main/java/App.groovy
+++ src/main/java/App.groovy
@@ -1,117 +1,107 @@
-import java.io.File
-import java.io.FileOutputStream
-import java.io.PrintStream;
-import java.io.PrintWriter
-import java.util.ArrayList;
-import java.util.logging.FileHandler;
-import java.util.logging.Handler;
-import java.util.logging.LogManager;
-import java.util.logging.Logger;
-import java.util.logging.SimpleFormatter;
-import java.util.regex.Matcher
-
-import util.LoggingOutputStream;
-import util.LoggerPrintStream;
-import util.StdOutErrLevel;
+import org.slf4j.Logger
+import org.slf4j.LoggerFactory
 
 class App {
+    private static final Logger LOGGER = LoggerFactory.getLogger(this);
 
-	def static run(){
-		//read csv and clone github repositories
-		Read r = new Read("projects.csv",false)
-		def projects = r.getProjects()
-		restoreGitRepositories(projects)
+    def static run() {
+        LOGGER.info("teste")
 
-		//fill merge scenarios info (base,left,right)
-		projects.each {
-			Extractor e = new Extractor(it,false)
-			e.fillAncestors()
-			println('Project ' + it.name + " read")
-		}
+        //read csv and clone github repositories
+        Read r = new Read("projects.csv", false)
+        def projects = r.getProjects()
+        restoreGitRepositories(projects)
 
-		//download scenarios
-		LinkedList<MergeCommit> horizontalExecutionMergeCommits = fillMergeCommitsListForHorizontalExecution(projects)
-		//horizontalExecutionMergeCommits = filter(horizontalExecutionMergeCommits)
-		for(int i=0; i<horizontalExecutionMergeCommits.size();i++){
-			MergeCommit m = horizontalExecutionMergeCommits.get(i);
-			println ('Downloading ' + ((i+1)+'/'+horizontalExecutionMergeCommits.size()) + ': ' +  m.sha)
-			Extractor ext = new Extractor(m)
-			ext.download_merge_scenario(m)
-		}
-		println 'Downloads Finished!'
-	}
+        //fill merge scenarios info (base,left,right)
+        projects.each {
+            Extractor e = new Extractor(it, false)
+            e.fillAncestors()
+            LOGGER.info('Project ' + it.name + " read")
+        }
 
-	def private static restoreGitRepositories(ArrayList<Project> projects){
-		projects.each {
-			Extractor e = new Extractor(it,true)
-			e.restoreWorkingFolder()
-		}
-		println('Restore finished!\n')
-	}
+        //download scenarios
+        LinkedList<MergeCommit> horizontalExecutionMergeCommits = fillMergeCommitsListForHorizontalExecution(projects)
+        //horizontalExecutionMergeCommits = filter(horizontalExecutionMergeCommits)
+        for (int i = 0; i < horizontalExecutionMergeCommits.size(); i++) {
+            MergeCommit m = horizontalExecutionMergeCommits.get(i);
+            LOGGER.info('Downloading ' + ((i + 1) + '/' + horizontalExecutionMergeCommits.size()) + ': ' + m.sha)
+            Extractor ext = new Extractor(m)
+            ext.download_merge_scenario(m)
+        }
+        LOGGER.info 'Downloads Finished!'
+    }
 
-	def private static LinkedList<MergeCommit> fillMergeCommitsListForHorizontalExecution(ArrayList<Project> projects){
-		LinkedList<MergeCommit> horizontalExecutionMergeCommits = new LinkedList<MergeCommit>()
-		int aux = projects.size()
-		int i 	= 0;
-		while(i < projects.size()) {
-			Project p = projects.get(i)
-			if(!p.listMergeCommit.isEmpty()){
-				MergeCommit mergeCommit = p.listMergeCommit.poll()
-				horizontalExecutionMergeCommits.add(mergeCommit)
-			}
-			if(p.listMergeCommit.isEmpty()){
-				projects.remove(i)
-			}
-			aux 	= projects.size()
-			if(aux == 0){
-				break
-			}
-			if(i >= (projects.size() - 1)){
-				i = 0;
-			} else {
-				i++;
-			}
-		}
-		Collections.shuffle(horizontalExecutionMergeCommits);
-		if(horizontalExecutionMergeCommits.size() > 10000){
-			return horizontalExecutionMergeCommits.subList(0, 10000);
-		} else {
-			return horizontalExecutionMergeCommits;
-		}
-	}
+    def private static restoreGitRepositories(ArrayList<Project> projects) {
+        projects.each {
+            Extractor e = new Extractor(it, true)
+            e.restoreWorkingFolder()
+        }
+        LOGGER.info('Restore finished!\n')
+    }
 
-	def private static fillExecutionLog(MergeCommit lastMergeCommit){
-		def out = new File('execution.log')
-		out.append (lastMergeCommit.projectName+','+lastMergeCommit.sha)
-		out.append '\n'
+    def private static LinkedList<MergeCommit> fillMergeCommitsListForHorizontalExecution(ArrayList<Project> projects) {
+        LinkedList<MergeCommit> horizontalExecutionMergeCommits = new LinkedList<MergeCommit>()
+        int aux = projects.size()
+        int i = 0;
+        while (i < projects.size()) {
+            Project p = projects.get(i)
+            if (!p.listMergeCommit.isEmpty()) {
+                MergeCommit mergeCommit = p.listMergeCommit.poll()
+                horizontalExecutionMergeCommits.add(mergeCommit)
+            }
+            if (p.listMergeCommit.isEmpty()) {
+                projects.remove(i)
+            }
+            aux = projects.size()
+            if (aux == 0) {
+                break
+            }
+            if (i >= (projects.size() - 1)) {
+                i = 0;
+            } else {
+                i++;
+            }
+        }
+        Collections.shuffle(horizontalExecutionMergeCommits);
+        if (horizontalExecutionMergeCommits.size() > 10000) {
+            return horizontalExecutionMergeCommits.subList(0, 10000);
+        } else {
+            return horizontalExecutionMergeCommits;
+        }
+    }
 
-		String a;
-		a.equalsIgnoreCase(a)
-	}
+    def private static fillExecutionLog(MergeCommit lastMergeCommit) {
+        def out = new File('execution.log')
+        out.append(lastMergeCommit.projectName + ',' + lastMergeCommit.sha)
+        out.append '\n'
 
-	def private static filter(List<MergeCommit> mergeCommits){
-		//TODO IMPLEMENT BELOW YOUR CRITERIA FOR FILTERING MERGE COMMITS
-		List<MergeCommit> filtered = new ArrayList<>()
-		File f = new File('filter.csv')
-		if(!f.exists() ) {
-			println "Filter file does not exist!"
-		} else {
-			for(MergeCommit m : mergeCommits){
-				f.eachLine { line ->
-					String[] a = line.split(';')
-					String pname = a[0]
-					String right = a[1]
-					String left  = a[2]
-					if(m.projectName.equalsIgnoreCase(pname) && m.parent2.startsWith(right) && m.parent1.startsWith(left)){
-						filtered.add(m)
-					}
-				}
-			}
-		}
-		return filtered
-	}
+        String a;
+        a.equalsIgnoreCase(a)
+    }
 
-	public static void main (String[] args){
-		run()
-	}
+    def private static filter(List<MergeCommit> mergeCommits) {
+        //TODO IMPLEMENT BELOW YOUR CRITERIA FOR FILTERING MERGE COMMITS
+        List<MergeCommit> filtered = new ArrayList<>()
+        File f = new File('filter.csv')
+        if (!f.exists()) {
+            LOGGER.info "Filter file does not exist!"
+        } else {
+            for (MergeCommit m : mergeCommits) {
+                f.eachLine { line ->
+                    String[] a = line.split(';')
+                    String pname = a[0]
+                    String right = a[1]
+                    String left = a[2]
+                    if (m.projectName.equalsIgnoreCase(pname) && m.parent2.startsWith(right) && m.parent1.startsWith(left)) {
+                        filtered.add(m)
+                    }
+                }
+            }
+        }
+        return filtered
+    }
+
+    public static void main(String[] args) {
+        run()
+    }
 }
--- src/main/java/Read.groovy
+++ src/main/java/Read.groovy
@@ -1,113 +1,115 @@
-import java.util.ArrayList;
+import org.slf4j.Logger
+import org.slf4j.LoggerFactory
 
 class Read {
+    private static final Logger LOGGER = LoggerFactory.getLogger(this);
 
-	private ArrayList<MergeCommit> listMergeCommit
-	private ArrayList<Project> listProject
-	private String csvCommitsFile
-	private String csvProjectsFile
+    private ArrayList<MergeCommit> listMergeCommit
+    private ArrayList<Project> listProject
+    private String csvCommitsFile
+    private String csvProjectsFile
 
-	public Read(fileName, boolean withGitMiner){
-		this.listMergeCommit = new ArrayList<MergeCommit>()
-		this.listProject 	 = new ArrayList<Project>()
-		this.csvProjectsFile = fileName
-		this.readProjectsCSV(withGitMiner)
-	}
+    public Read(fileName, boolean withGitMiner) {
+        this.listMergeCommit = new ArrayList<MergeCommit>()
+        this.listProject = new ArrayList<Project>()
+        this.csvProjectsFile = fileName
+        this.readProjectsCSV(withGitMiner)
+    }
 
-	def setCsvCommitsFile(fileName){
-		this.csvCommitsFile = fileName
-	}
+    def setCsvCommitsFile(fileName) {
+        this.csvCommitsFile = fileName
+    }
 
-	def readCommitsCSV() {
-		BufferedReader br = null
-		String line 	  = ""
-		String csvSplitBy = ","
-		try {
-			br = new BufferedReader(new FileReader(this.csvCommitsFile))
-			br.readLine()
-			while ((line = br.readLine()) != null) {
-				String[] shas = line.split(csvSplitBy)
+    def readCommitsCSV() {
+        BufferedReader br = null
+        String line = ""
+        String csvSplitBy = ","
+        try {
+            br = new BufferedReader(new FileReader(this.csvCommitsFile))
+            br.readLine()
+            while ((line = br.readLine()) != null) {
+                String[] shas = line.split(csvSplitBy)
 
-				def mergeCommit = new MergeCommit()
-				mergeCommit.sha 	= shas[0]
-				mergeCommit.parent1 = shas[1]
-				mergeCommit.parent2 = shas[2]
+                def mergeCommit = new MergeCommit()
+                mergeCommit.sha = shas[0]
+                mergeCommit.parent1 = shas[1]
+                mergeCommit.parent2 = shas[2]
 
-				this.listMergeCommit.add(mergeCommit)
+                this.listMergeCommit.add(mergeCommit)
 
-				println ("SHA's [MergeCommit= " + mergeCommit.sha 	+ " , Parent1=" + mergeCommit.parent1 + " , Parent2=" + mergeCommit.parent2 +  "]")
-			}
+                LOGGER.info("SHA's [MergeCommit= " + mergeCommit.sha + " , Parent1=" + mergeCommit.parent1 + " , Parent2=" + mergeCommit.parent2 + "]")
+            }
 
-		} catch (FileNotFoundException e) {
-			e.printStackTrace()
-		} catch (IOException e) {
-			e.printStackTrace()
-		} finally {
-			if (br != null) {
-				try {
-					br.close();
-				} catch (IOException e) {
-					e.printStackTrace()
-				}
-			}
-		}
-	}
+        } catch (FileNotFoundException e) {
+            e.printStackTrace()
+        } catch (IOException e) {
+            e.printStackTrace()
+        } finally {
+            if (br != null) {
+                try {
+                    br.close();
+                } catch (IOException e) {
+                    e.printStackTrace()
+                }
+            }
+        }
+    }
 
-	def readProjectsCSV(boolean withGitMiner) {
-		BufferedReader br = null
-		String line = ""
-		String csvSplitBy = ","
-		try {
-			br = new BufferedReader(new FileReader(this.csvProjectsFile))
-			br.readLine()
-			while ((line = br.readLine()) != null) {
-				String[] info = line.split(csvSplitBy)
+    def readProjectsCSV(boolean withGitMiner) {
+        BufferedReader br = null
+        String line = ""
+        String csvSplitBy = ","
+        try {
+            br = new BufferedReader(new FileReader(this.csvProjectsFile))
+            br.readLine()
+            while ((line = br.readLine()) != null) {
+                String[] info = line.split(csvSplitBy)
 
-				def project 	= new Project()
-				project.name 	= info[0]
-				project.url 	= info[1]
-				
-				if(!withGitMiner && info.length == 4) {
-					project.miningSinceDate = info[2]
-					project.miningUntilDate = info[3]
-				}
-				if(withGitMiner)
-					project.graph 	= info[2]
+                def project = new Project()
+                project.name = info[0]
+                project.url = info[1]
 
-				this.listProject.add(project)
+                if (!withGitMiner && info.length == 4) {
+                    project.miningSinceDate = info[2]
+                    project.miningUntilDate = info[3]
+                }
+                if (withGitMiner)
+                    project.graph = info[2]
 
-				if(withGitMiner)
-					println ("PROJECT [Name= " + project.name + " , Url=" + project.url + " , Graph dir=" + project.graph +  "]")
-				else
-					println ("PROJECT [Name= " + project.name + " , Url=" + project.url +  "]")
+                this.listProject.add(project)
 
-			}
+                if (withGitMiner)
+                    LOGGER.info("PROJECT [Name= " + project.name + " , Url=" + project.url + " , Graph dir=" + project.graph + "]")
+                else
+                    LOGGER.info("PROJECT [Name= " + project.name + " , Url=" + project.url + "]")
 
-		} catch (FileNotFoundException e) {
-			e.printStackTrace()
-		} catch (IOException e) {
-			e.printStackTrace()
-		} finally {
-			if (br != null) {
-				try {
-					br.close();
-				} catch (IOException e) {
-					e.printStackTrace()
-				}
-			}
-		}
-	}
+            }
 
-	def ArrayList<MergeCommit> getMergeCommitsList(){
-		return this.listMergeCommit
-	}
+        } catch (FileNotFoundException e) {
+            e.printStackTrace()
+        } catch (IOException e) {
+            e.printStackTrace()
+        } finally {
+            if (br != null) {
+                try {
+                    br.close();
+                } catch (IOException e) {
+                    e.printStackTrace()
+                }
+            }
+        }
+    }
 
-	def ArrayList<Project> getProjects(){
-		return this.listProject
-	}
+    def ArrayList<MergeCommit> getMergeCommitsList() {
+        return this.listMergeCommit
+    }
 
-	//	static void main(args) {
-	//		Reader obj = new Reader("commits.csv")
-	//		obj.readCSV()
-	//	}
+    def ArrayList<Project> getProjects() {
+        return this.listProject
+    }
+
+    //	static void main(args) {
+    //		Reader obj = new Reader("commits.csv")
+    //		obj.readCSV()
+    //	}
 }
